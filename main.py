b388599965b4f796220f2ed28d69a11709cf2811
uumaaa <ikerantonioplumaamro@gmail.com>
2023-09-24 14:08:39 -0600

    componentes conectados

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from Binarización import bersen
+from Thresholding import bersen
 from Color import space_color
+from Components import component_detection
 import cv2 as cv
 import matplotlib.pyplot as plt
-''' img = cv.imread('person_bacteria.jpeg',cv.IMREAD_GRAYSCALE)
-img2 = BersenThreshold(img,13,30,210) /'''
 
 if __name__ == "__main__":
     # Lectura de imágenes
@@ -11,9 +10,16 @@ if __name__ == "__main__":
     img2 = cv.imread("image-cell.png",cv.IMREAD_GRAYSCALE)
     img3 = cv.imread("person_bacteria.jpeg",cv.IMREAD_GRAYSCALE)
     # Umbralizacion adaptativa
-    img1B = bersen.BersenThreshold(img1,2,60,136)
-    img2B = bersen.BersenThreshold(img2,2,45,240,2)
-    img3B = bersen.BersenThreshold(img3,11,40,200)
+    img1B = bersen.BersenThreshold(img1,2,30,147)
+    img2B = bersen.BersenThreshold(img2,2,35,250,2)
+    img3B = bersen.BersenThreshold(img3,19,35,186)
+    img2B = space_color.invert_binary(img2B)
+    #Deteccion de componentes 
+
+    img1BC = component_detection.conntected_components(img1B)
+    img2BC = component_detection.conntected_components(img2B)
+    img3BC = component_detection.conntected_components(img3B)
+
     plt.figure(1,figsize=(8,8))
     plt.subplot(3,4,1)
     plt.imshow(img1,cmap="gray")
@@ -44,5 +50,34 @@ if __name__ == "__main__":
     plt.imshow(img3B,cmap="gray")
     plt.axis("off")
     plt.title("Umbralización Bernsen")
+    plt.figure(2,figsize=(8,8))
+    plt.subplot(3,4,1)
+    plt.imshow(img1,cmap="gray")
+    plt.axis("off")
+    plt.title("Imagen original")
+    plt.subplot(3,4,2)
+    plt.axis("off")
+    plt.title("Componentes U. Bernsen")
+    plt.imshow(img1BC,cmap="nipy_spectral")
+    plt.subplot(3,4,3)
+    plt.axis("off")
+    plt.subplot(3,4,5)
+    plt.imshow(img2,cmap="gray")
+    plt.axis("off")
+    plt.title("Imagen original")
+    plt.subplot(3,4,6)
+    plt.axis("off")
+    plt.title("Componentes U. Bernsen")
+    plt.imshow(img2BC,cmap="nipy_spectral")
+    plt.subplot(3,4,6)
+    plt.axis("off")
+    plt.subplot(3,4,9)
+    plt.axis("off")
+    plt.imshow(img3,cmap="gray")
+    plt.axis("off")
+    plt.title("Imagen original")
+    plt.subplot(3,4,10)
+    plt.imshow(img3BC,cmap="nipy_spectral")
+    plt.axis("off")
+    plt.title("Componentes U. Bernsen")
     plt.show()
-
